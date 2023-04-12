5dcc7846fad53f5676650376dc148c6de9c87b5d
Taher Mandapwala <mandapwalataher@gmail.com>
2023-04-12 17:33:31 +0530

    Added test case for the same

--- first_project/src/main/java/com/example/first_project/FirstProjectApplication.java
+++ first_project/src/main/java/com/example/first_project/FirstProjectApplication.java
@@ -1,13 +1,13 @@
 package com.example.first_project;
 
-import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FirstProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstProjectApplication.class, args);
+		// SpringApplication.run(FirstProjectApplication.class, args);
 		System.out.println("Welcome To Roost/TestGPT....");
 	}
 
--- first_project/src/test/java/com/example/first_project/FirstProjectApplicationTests.java
+++ first_project/src/test/java/com/example/first_project/FirstProjectApplicationTests.java
@@ -1,13 +1,51 @@
 package com.example.first_project;
 
+import java.util.*;
+
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FirstProjectApplicationTests {
 
+	ArrayList<String> obj = new ArrayList<String>();
+
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	public void testAdd() {
+		obj.add("Emma");
+		obj.add("Ronan");
+		obj.add("Antonio");
+		obj.add("Paul");
+		System.out.println("Adding 4 student to list: ");
+		assertEquals(4, obj.size());
+	}
+
+	@Test
+	public void testSize() {
+		obj.add("Emma");
+		obj.add("Ronan");
+		obj.add("Antonio");
+		System.out.println("Checking size of List: ");
+		assertEquals(3, obj.size());
+	}
+
+	@Test
+	public void testRemove() {
+		obj.add("Antonio");
+		obj.add("Paul");
+		obj.remove("Paul");
+		System.out.println("Removing 1 student from list: ");
+		assertEquals(1, obj.size());
+	}
+
+	// @Test
+	// public void removeAll() {
+	// obj.removeAll();
+	// }
 
 }
